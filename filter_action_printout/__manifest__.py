f5bf2b35629eb2c321d245f745c6da8e7a2de3cf
rifqi <rifqi.artsys@gmail.com>
2022-12-23 09:33:18 +0700

    Add filter_action_printout

---
+++ filter_action_printout/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Filter Action Printout',
+    'version': '14.0.1.0.0',
+    'summary': 'Filter printout actions for visible and invisible',
+    'author': 'Newbie Dev',
+    'website': 'https://agvenmuharisrifqi.github.io',
+    'license': 'AGPL-3',
+    'category': 'Custom/Custom',
+    'depends': [
+        'base', 'web'
+    ],
+    'data': [
+        'assets/assets.xml',
+        'views/ir_actions_report.xml',
+    ],
+    "installable": True,
+    'images': [
+        'static/description/after_filter.png',
+    ]
+}
