82e42aeab86bb085e8ff779b4c08fb5d7b07e3b6
rifqi <rifqi.artsys@gmail.com>
2023-06-05 11:08:39 +0700

    INIT hide_or_show_password

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# odoo_free_modules
+![Odoo v13.0](https://img.shields.io/badge/build-v13.0-9925be)
+[![Odoo Apps](https://img.shields.io/badge/master-apps-success)](https://apps.odoo.com/apps/browse?repo_maintainer_id=496549)
+
 Repository for Odoo Module Free
+-------------------------------
+
+Repository which is used to store the odoo modules that We made and is free for you to use.
+
+Please support us with
+- [x] Don't forget to give stars
+- [x] Download our app
+
+
+> **Odoo v13.0**
---
+++ hide_or_show_password/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Hide Or Show Password',
+    'version': '13.0.1.0.0',
+    'summary': """
+        Show and Hide fields by typing a password, login form, signup form, user change password
+    """,
+    'author': 'Newbie Dev',
+    'website': 'https://agvenmuharisrifqi.github.io',
+    'license': 'AGPL-3',
+    'category': 'Custom/Custom',
+    'depends': [
+        'web', 'base', 'auth_signup'
+    ],
+    'data': [
+        'assets/assets.xml',
+        'views/login_signup.xml',
+    ],
+    'qweb': [
+        'static/src/xml/profile.xml',
+    ],
+    'installable': True,
+    'images':  ['static/description/banner.png'],
+}
